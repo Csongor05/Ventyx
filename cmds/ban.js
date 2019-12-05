7c418ed41bbe70d33abda4ac10c9d3efc39f9191
Csongor05 <54900889+Csongor05@users.noreply.github.com>
2019-12-05 22:16:55 +0200

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -5,18 +5,18 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
 
 let xdemb = new Discord.RichEmbed()
-        .setColor("#00ff00")
+        .setColor("#cf8810")
         .setTitle("Ban Parancs")
-        .addField("Leírás:", `Kitiltja a felhasználot`, true)
-        .addField("Használat:", `f.ban <említés> <indok>`, true)
-        .addField("Példa:", `f.ban @Csongor Hirdetés`)
+        .addField("Leírás:", `Kitiltja a felhasználót`, true)
+        .addField("Használat:", `v.ban <említés> <indok>`, true)
+        .addField("Példa:", `v.ban @Csongor Hirdetés`)
 
         if(!message.member.hasPermission("BAN_MEMBERS") && message.author.id !== "366992986896203780") return message.channel.send(":x: Nincs jogod használni a parancsot!");
 
         let member = message.mentions.members.first();
         if(!member) return message.channel.send(xdemb)
         if(!member.bannable) return message.channel.send(":x: Nem tudtam kitiltani a felhasználot!")
-        if(member.user.id === "291221132256870400") return message.channel.send(":x: Nem tudtam kitiltani a **Készítőmet**!")
+        if(member.user.id === "366992986896203780") return message.channel.send(":x: Nem tudtam kitiltani a **Készítőmet**!")
 
         if(member.id === message.author.id) return message.channel.send(":x: Nem tilthatod ki saját magad!")
 
@@ -31,7 +31,7 @@ let xdemb = new Discord.RichEmbed()
         await member.ban(reason).catch(error => message.channel.send(`:x: Nem tudtam kitiltani mert: ${error}`));
 
         let bean = new Discord.RichEmbed()
-        .setColor("#00ff00")
+        .setColor("#a234b4")
         .setTitle(`Kitiltva | ${member.user.tag}`)
         .addField("Felhasználó:", member, true)
         .addField("Általa:", message.author, true)
